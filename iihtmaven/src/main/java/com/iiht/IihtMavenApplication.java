488e57781b7960c80d2ff6454a7e25e8c263cbd2
Satya <satyavinn@gmail.com>
2019-06-10 21:07:43 +0530

    iihtmaven project first commit

---
+++ iihtmaven/src/main/java/com/iiht/IihtMavenApplication.java
@@ -0,0 +1,13 @@
+package com.iiht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IihtMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IihtMavenApplication.class, args);
+	}
+
+}
